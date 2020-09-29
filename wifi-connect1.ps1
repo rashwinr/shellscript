f1affba5b7bf3ac016792cb7ff58f045f44c2eb9
Ashwin Raj Kumar <ark440@nyu.edu>
2020-09-28 22:36:50 -0400

    Update wifi-connect1.ps1

--- wifi-connect1.ps1
+++ wifi-connect1.ps1
@@ -4,7 +4,7 @@ $online = test-connection 8.8.8.8 -Count 1 -Quiet
 if ($online)               #If to test online
 {
 echo "Connected and has internet"      #echo to terminal
-Start-Sleep -s 180     #Sleep for 5 minutes
+Start-Sleep -s 300     #Sleep for 5 minutes
 netsh wlan disconnect  #disconnect after 5 minutes
 }
 else
