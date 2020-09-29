597220640072de3c8abd7cbde2a6714d6b47b037
Ashwin Raj Kumar <ark440@nyu.edu>
2020-09-28 22:21:19 -0400

    Update wifi-connect1.ps1

--- wifi-connect1.ps1
+++ wifi-connect1.ps1
@@ -1,11 +1,13 @@
 C:\Users\User\Desktop\wifi\wifi-connect.ps1
-Start-Sleep 1
+Start-Sleep 1              #Wait until connection is complete
 $online = test-connection 8.8.8.8 -Count 1 -Quiet
-if ($online)
+if ($online)               #If to test online
 {
-echo "Connected and has internet"
+echo "Connected and has internet"      #echo to terminal
+Start-Sleep -s 180     #Sleep for 5 minutes
+netsh wlan disconnect  #disconnect after 5 minutes
 }
 else
 {
-netsh wlan connect name="VMF4H"
+netsh wlan connect name="VMF4H"        #Connecting to a previous network in profile
 } 
